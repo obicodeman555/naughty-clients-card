c6cdfcbf6dec08220635861e6c9543531936a68b
obicodeman555 <obicodeman@yahoo.com>
2020-07-17 21:40:47 +0100

    Updates

---
+++ precache-manifest.2e1cdb5723afc82aafa0c9c879960731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bacb850dc9aa848dff00a89d3083e49",
+    "url": "/naughty-clients-card/index.html"
+  },
+  {
+    "revision": "c3c11e1d0a800f9c1b64",
+    "url": "/naughty-clients-card/static/css/main.283087ae.chunk.css"
+  },
+  {
+    "revision": "2aa89dde0859bdbbf2dc",
+    "url": "/naughty-clients-card/static/js/2.c3b51709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/naughty-clients-card/static/js/2.c3b51709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c11e1d0a800f9c1b64",
+    "url": "/naughty-clients-card/static/js/main.fb078992.chunk.js"
+  },
+  {
+    "revision": "f36c33b5fdb10303a585",
+    "url": "/naughty-clients-card/static/js/runtime-main.d74159f7.js"
+  }
+]);
